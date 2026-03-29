c98d9383a843c2c91cc1e6ee42cd5c95705cf7af
Mirianne dos Santos <mirianneds@gmail.com>
2026-03-29 17:28:43 +0200

    Añadido script creación de la base de datos

---
+++ Bases_de_Datos/SQL/Estructura_ BBDD_ Neverland.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE Neverland;
+
+CREATE TABLE Cliente (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE Pago (
+    id_pago INT PRIMARY KEY AUTO_INCREMENT,
+    importe DECIMAL(10,2) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    id_cliente INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
+
+CREATE TABLE Reserva (
+   id_reserva INT PRIMARY KEY AUTO_INCREMENT,
+   fecha_reserva DATE NOT NULL,
+   id_pago INT NOT NULL,
+   FOREIGN KEY (id_pago), REFERENCES Pago(id_pago)
+);
+
+CREATE TABLE Evento (
+   id_evento INT PRIMARY KEY AUTO_INCREMENT,
+   fecha_evento DATE NOT NULL,
+   turno VARCHAR(50) NOT NULL,
+   tipo VARCHAR(50) NOT NULL,
+   limpieza BOOLEAN NOT NULL,
+   id_reserva INT UNIQUE NOT NULL,
+   FOREIGN KEY (id_reserva), REFERENCES Reserva(id_reserva)
+);
